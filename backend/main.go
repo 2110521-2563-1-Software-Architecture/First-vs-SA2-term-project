429fd5e9d0eb2a1b1337934770d61bcb2af5f482
zpoom <poom007.sk135@gmail.com>
2020-10-07 20:10:24 +0700

    Remove mongo connection

--- backend/main.go
+++ backend/main.go
@@ -1,30 +1,11 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
-
-func connectMongo() (c *Client) {
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://url-matcher:EeyFLrO0PLRrqXDe@cluster0.kj1oc.gcp.mongodb.net/url-matcher?retryWrites=true&w=majority"))
-	if err != nil {
-		fmt.Println(err)
-		// return
-	}
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	err = client.Connect(ctx)
-	if err != nil {
-		fmt.Println(err)
-	}
-	return client
-	client.Database('url-matcher').
-}
 
 type RequestBody struct {
 	URL string `json:"url"`
