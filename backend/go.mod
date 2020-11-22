e8f8c156e52dfb3eaeb360c2ccd6d00d03cb34fe
Nisaruj Rattanaaram <nisaruj@hotmail.com>
2020-11-22 13:05:14 +0700

    Add redis cache

--- backend/go.mod
+++ backend/go.mod
@@ -5,4 +5,5 @@ go 1.15
 require (
 	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.6.3
+	github.com/go-redis/redis/v8 v8.4.0
 )
--- backend/main.go
+++ backend/main.go
@@ -4,12 +4,32 @@ import (
 	"github.com/2110521-2563-1-Software-Architecture/First-vs-SA2-term-project/handlers"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
 )
 
+func RedisClient() *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+	
+	return client
+}
+
 func main() {
+	rClient := RedisClient()
+
 	router := gin.Default()
 	router.Use(cors.Default())
+	router.Use(Redis(rClient))
 	router.POST("/shorten", handlers.ShortenURL)
 	router.GET("/:hash", handlers.Redirect)
 	router.Run(":8080")
 }
+
+func Redis(client *redis.Client) gin.HandlerFunc {
+	return func(c *gin.Context) {
+			c.Set("redis", client)
+			c.Next()
+	}
+}
+
